fb3428b177a1d9577c19518e206850f45614b440
Artiom Kuciuk <kuchuk.artiom@gmail.com>
2019-09-24 07:23:22 +0200

    Paste templates into active window

--- Wox.Plugin.Links/Services/ClipboardService.cs
+++ Wox.Plugin.Links/Services/ClipboardService.cs
@@ -1,25 +1,49 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace Wox.Plugin.Links.Services {
     public interface IClipboardService {
         void SetText(string text);
+
         string GetText();
     }
 
     public class ClipboardService : IClipboardService {
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        public static void PasteToApplication() {
+            var handle = GetForegroundWindow();
+            if (handle == IntPtr.Zero) {
+                return;
+            }
+            SetForegroundWindow(handle);
+            SendKeys.SendWait("^v");
+        }
+
         public void SetText(string text) {
-            Execute(() => Clipboard.SetText(text));
+            Execute(() => PasteText(text), false);
+        }
+
+        private static void PasteText(string text) {
+            Clipboard.SetText(text);
+            Thread.Sleep(150);
+            PasteToApplication();
         }
 
         public string GetText() {
             var text = @"";
-            Execute(() => text = Clipboard.GetText());
+            Execute(() => text = Clipboard.GetText(), true);
             return text;
         }
 
-        private void Execute(Action action) {
+        private void Execute(Action action, bool wait) {
             Thread staThread = new Thread(
                 delegate() {
                     try {
@@ -30,7 +54,8 @@ namespace Wox.Plugin.Links.Services {
                 });
             staThread.SetApartmentState(ApartmentState.STA);
             staThread.Start();
-            staThread.Join();
+            if (wait)
+                staThread.Join();
         }
     }
 }
